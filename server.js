0517addf6c5d8b8dc4e514835f0030df92a3e62b
Nelluru-Hemasree <hemasree.nelluru@zemosolabs.com>
2021-11-15 19:43:02 +0530

    add cors

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const data = require("./data");
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
 
 server.get("/users/logout", (req, res) => {
   res.json({ message: "success" });
